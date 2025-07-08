047d8f87d2c257fc79fba0d4cdac13d7676b5531
eidabdallah <eidabdallah971@gmail.com>
2025-07-09 00:04:27 +0300

    create cache class

---
+++ src/utils/cache.ts
@@ -0,0 +1,18 @@
+import NodeCache from 'node-cache';
+
+class Cache {
+  private static cache = new NodeCache({ stdTTL: 900 });
+  static get<T>(key: string): T | undefined {
+    return this.cache.get<T>(key);
+  }
+  static set<T>(key: string, value: T): boolean {
+    return this.cache.set(key, value);
+  }
+  static del(key: string): void {
+    this.cache.del(key);
+  }
+  static flush(): void {
+    this.cache.flushAll();
+  }
+}
+export default Cache;
